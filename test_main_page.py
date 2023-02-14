3a35e27c2bd4ad0e07e574f39892e653ea7ff1a3
Михаил Яковенко <yakovenki@yandex.ru>
2023-02-14 18:30:19 +0300

    вынесли селектор в locators.py

--- test_main_page.py
+++ test_main_page.py
@@ -1,7 +1,7 @@
-from .pages.main_page import MainPage
+from pages.main_page import MainPage
 
 
-link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer'
+link = "http://selenium1py.pythonanywhere.com/"
 
 
 def test_guest_can_go_to_login_page(browser):
