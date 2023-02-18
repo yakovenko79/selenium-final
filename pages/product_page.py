8a021a79625097b3baaf03df0d25ee28dbcc783e
Михаил Яковенко <yakovenki@yandex.ru>
2023-02-18 20:06:21 +0300

    проверка работоспособности кнопки добавить в корзину

---
+++ pages/product_page.py
@@ -0,0 +1,42 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+
+class ProductPage(BasePage):
+
+    def go_to_basket_page(self):
+        # нажимаем кнопку "add product"
+        basket_link = self.browser.find_element(*ProductPageLocators.PRODUCT_LINK)
+        basket_link.click()
+
+    def is_message_after_adding(self):
+        # проверяем присутствуют ли название товара и сообщение о добавленном товаре на странице
+        assert self.is_element_present(*ProductPageLocators.PRODUCT_NAME), "Product name is not on the page"
+        assert self.is_element_present(*ProductPageLocators.IN_BASKET_MESSAGE), "Link about adding is not on the page"
+
+        # получаем текст названия товара и текст сообщения о добавлении товара
+        product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
+        basket_message = self.browser.find_element(*ProductPageLocators.IN_BASKET_MESSAGE).text
+
+        # проверяем, что название товара присутствует в сообщении о добавлении товара
+        assert product_name in basket_message, "Product name is not in message"
+
+
+    def should_be_message_about_basket_total(self):
+
+        # проверяем присутствуют ли элементы на странице
+        assert self.is_element_present(*ProductPageLocators.PRODUCT_COST), "Price is not on the page"
+        assert self.is_element_present(*ProductPageLocators.IN_BASKET_TOTAL), "Price is not in basket"
+
+        # получаем текст цены товара и текст стоимости товаров в корзине
+        product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_COST).text
+        basket_total = self.browser.find_element(*ProductPageLocators.IN_BASKET_TOTAL).text
+
+        # сравниваем текст цены товара и текст стоимости товаров в корзине
+        assert product_price == basket_total, "Price is not equal"
+
+
+
+
+
+
