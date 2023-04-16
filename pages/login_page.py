00d2f2596d162e793048b7b980aa14f5e9480eec
Михаил Яковенко <yakovenki@yandex.ru>
2023-04-16 09:40:36 +0300

    initialize login page

--- pages/login_page.py
+++ pages/login_page.py
@@ -10,7 +10,7 @@ class LoginPage(BasePage):
 
     def should_be_login_url(self):
         # реализуйте проверку на корректный url адрес
-        assert "login" in self.browser.current_url, "login not in url"
+        assert "login" in self.browser.current_url, "login is not in url"
 
     def should_be_login_form(self):
         assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not on the page"
